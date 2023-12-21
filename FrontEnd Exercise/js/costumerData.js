382b1738183b154e14e3e44f9ed5aeea196d59ba
Jose Daniel Oropeza Soria <danioropezasoria@gmail.com>
2023-12-21 10:10:26 -0400

    Remove duplicated agent script in Intro Script.

--- FrontEnd Exercise/js/costumerData.js
+++ FrontEnd Exercise/js/costumerData.js
@@ -50,11 +50,11 @@
   updateIntroScript();
 
   function updateIntroScript() {
-    document.getElementById("intro-paragraph-1").innerHTML = `Agent:Hey there! I'm FJorian. Welcome to Nobel!. How's your day going, <span class="highlight">${firstName} ${lastName}</span>?`
+    document.getElementById("intro-paragraph-1").innerHTML = `Agent: Hey there! I'm FJorian. Welcome to Nobel!. How's your day going, <span class="highlight">${firstName} ${lastName}</span>?`
     document.getElementById("intro-paragraph-2").innerHTML = `Costumer: Hey FJorian! It's good, thanks. How about yours?`
     document.getElementById("intro-paragraph-3").innerHTML = `Agent: Glad to hear that! My day's going well, especially now that I get to chat with someone like you. Your experience at <span class="highlight">${company}</span> caught my eye. We're growing our IT department in <span class="highlight">${state}, ${city}</span> and I think your skills could be a game-changer for us. What do you think?`
     document.getElementById("intro-paragraph-4").innerHTML = `Customer: Oh, really? That's intriguing! Tell me more.`;
-    document.getElementById("intro-paragraph-5").innerHTML = `Agent: Absolutely! I'd love to dive into the details. How about we schedule a quick follow-up to chat about how your expertise could fit right in here at Stellar? What time works for you?Agent: Absolutely! I'd love to dive into the details. How about we schedule a quick follow-up to chat about how your expertise could fit right in here at Nobel? What time works for you?`;
+    document.getElementById("intro-paragraph-5").innerHTML = `Agent: Absolutely! I'd love to dive into the details. How about we schedule a quick follow-up to chat about how your expertise could fit right in here at Stellar? What time works for you?`;
     document.getElementById("intro-paragraph-6").innerHTML = `Customer: Sounds good! How about tomorrow afternoon?`;
     document.getElementById("intro-paragraph-7").innerHTML = `Agent: Perfect! I'll lock that in. Can't wait to explore this further with you, <span class="highlight">${firstName} ${lastName}</span>!`;
   }
